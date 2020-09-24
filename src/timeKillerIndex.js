16a9fc183c7545e9613a252e1bb845f128cccd87
unknown <mm810304@gmail.com>
2020-09-24 22:43:26 +0900

    fixed link to backend in hangman app

--- src/timeKillerIndex.js
+++ src/timeKillerIndex.js
@@ -5,7 +5,7 @@ import Hangman from './timekillers/hangman.js';
 const id = window.location.pathname.replace('/time-killers/', '');
 
 async function getAppName(id) {
-    const response = await fetch(`https://mrmcgory/time-killers/${id}/app-name`, {
+    const response = await fetch(`https://mrmcgory.com/time-killers/${id}/app-name`, {
         headers: {
             Accept: 'application/json'
         }
